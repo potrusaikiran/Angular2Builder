2254e7408aff8e1883c76d2d93f86d8dadc1895c
saikiran <saikiran@ziraff.in>
2018-02-13 12:51:08 +0530

    changes in home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,8 +14,8 @@ export class HomeComponent implements OnInit {
   { 'name': 'Rajesh', 'bio': 'Guntur', 'job': 'developer', 'salary': 10 }];
   user: User;
   debugger;
-  isclick: boolean=false;
-  isgrid: boolean=true
+  isclick: boolean = false;
+  isgrid: boolean = true
   constructor(private _sharedService: SharedService) {
   }
   ngOnInit() {
@@ -23,28 +23,39 @@ export class HomeComponent implements OnInit {
   openpop() {
     this.isclick = true;
     this.user = new User(null, null, null, 0);
-    this.isgrid=false;
+    this.isgrid = false;
     // alert('UnderConstruction...!!!');
   }
   SelectedUser(user: User) {
     this.isclick = true;
-    this.isgrid=false
+    this.isgrid = false
     this.user = user;
   }
   addtolist(user: User) {
-    if(user.name==null||user.bio==null||user.job==null||user.salary==null)
-    alert("Please fill the fields to continue...!!!");
-    else{
-      this.users.forEach(use => {
-        if(use.name!=user.name)
-        this.users.push(user);
-        this.isgrid=true;
-      });
+    if (user.name == null || user.bio == null || user.job == null || user.salary == null)
+      alert("Please fill the fields to continue...!!!");
+    else {
+
+      this.users.push(user);
+      this.isgrid = true;
+      user = null;
     }
   }
-  cancel(user:User){
-    this.isgrid=true;
-    this.user=null;
+  cancel(user: User) {
+    this.isgrid = true;
+    this.user = null;
+  }
+  delete(user: any) {
+    if(user==null)
+    alert("please select checkbox")
+    else{
+      for (let i = 0; i < this.users.length; i++) {
+        if (this.users[i].name == user.name) {
+          this.users.splice(i, 1);
+          this.isgrid=true;
+        }
+      }
+    }
   }
 
 }
